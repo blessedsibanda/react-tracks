1f2ae2cb97bfb1f80b62536f73d532354c2c3002
Blessed Sibanda <n01519451g@students.nust.ac.zw>
2020-02-19 00:11:43 +0200

    only show fields that are absolutely necessary

--- app/users/schema.py
+++ app/users/schema.py
@@ -6,6 +6,7 @@ from graphene_django import DjangoObjectType
 class UserType(DjangoObjectType):
     class Meta:
         model = get_user_model()
+        only_fields = ('id', 'email', 'password', 'username')
 
 
 class CreateUser(graphene.Mutation):
